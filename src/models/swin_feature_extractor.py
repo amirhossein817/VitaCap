456e080c04a9064971e3eadc30826e2e04dd8ccf
Amirhossein <hosseinnia817@gmail.com>
2024-12-12 20:16:54 +0330

    Delete usless function

--- src/models/swin_feature_extractor.py
+++ src/models/swin_feature_extractor.py
@@ -52,18 +52,3 @@ class SwinFeatureExtractor(FeatureExtractor):
 
         return output
     
-
-# Define a feature extraction function
-def extract_features(model, image_path):
-    preprocess = transforms.Compose([
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
-    image = Image.open(image_path).convert('RGB')
-    input_tensor = preprocess(image).unsqueeze(0)  # Add batch dimension
-    
-    with torch.no_grad():
-        features = model.forward_features(input_tensor)  # Use forward_features for embeddings
-    return features
-
